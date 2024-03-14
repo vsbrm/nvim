977f4f81dbc9b88041de605813ae39e3dd3add59
Vasilii <bormyshevv@gmail.com>
2024-03-14 12:05:01 +0200

    Add keymapping for window nvigation

--- init.lua
+++ init.lua
@@ -12,4 +12,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("editor-config")
+require("window-config")
 require("lazy").setup("plugins")
---
+++ lua/window-config.lua
@@ -0,0 +1,8 @@
+vim.keymap.set('n', '<leader>wv', '<C-w>v', {desc = "vertical split"})
+vim.keymap.set('n', '<leader>ws', '<C-w>s', {desc = "split"})
+vim.keymap.set('n', '<leader>wq', '<C-w>q', {desc = "quit window"})
+vim.keymap.set('n', '<leader>wh', '<C-w>h', {desc = "go left"})
+vim.keymap.set('n', '<leader>wj', '<C-w>j', {desc = "go down"})
+vim.keymap.set('n', '<leader>wk', '<C-w>k', {desc = "go up"})
+vim.keymap.set('n', '<leader>wl', '<C-w>l', {desc = "go right"})
+vim.keymap.set('n', '<leader>ww', '<C-w>w', {desc = "switch window"})
