ece8443e28f0c8bbbcbdd8e728408ccd28280be7
vsb <bormyshevv@gmail.com>
2024-05-26 23:32:51 +0300

    Add window-resizing commands

--- lua/window-config.lua
+++ lua/window-config.lua
@@ -13,3 +13,8 @@ wk.register({
     w = { '<C-w>w', "switch window"}
   }
 }, { prefix = '<leader>'})
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
