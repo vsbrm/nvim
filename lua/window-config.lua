96a9e8dea34e04e06d0ba83a39dae4a0b31c6750
vsb <bormyshevv@gmail.com>
2024-07-17 23:31:20 +0300

    Update window keybindings

--- lua/window-config.lua
+++ lua/window-config.lua
@@ -1,20 +1,8 @@
-local wk = require('which-key')
-
-wk.register({
-  w = {
-    name = 'window',
-    v = { '<C-w>v', "vertical split"},
-    s = { '<C-w>s', "split"},
-    q = { '<C-w>q', "quit window"},
-    h = { '<C-w>h', "go left"},
-    j = { '<C-w>j', "go down"},
-    k = { '<C-w>k', "go up"},
-    l = { '<C-w>l', "go right"},
-    w = { '<C-w>w', "switch window"}
-  }
-}, { prefix = '<leader>'})
-
 vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Window go left" })
+vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Window go down" })
+vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Window go up" })
+vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Window go right" })
