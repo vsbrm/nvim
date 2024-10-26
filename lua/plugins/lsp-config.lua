19ad8639c6c8e3c64eee48ccdf23da95def163e5
vsb <bormyshevv@gmail.com>
2024-10-26 14:41:14 +0300

    Some lsp config improvement

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,14 +2,17 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require('lspconfig')
-      lspconfig.clangd.setup({
-       capabilities = capabilities
+      lspconfig.clangd.setup({})
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          vim.keymap.set('n', '<leader>ch', vim.lsp.buf.hover, { buffer = args.buf })
+          vim.keymap.set('n', '<leader>cr', vim.lsp.buf.references, { buffer = args.buf })
+          vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation, { buffer = args.buf })
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = args.buf })
+        end,
       })
-      lspconfig.lua_ls.setup({
-       capabilities = capabilities
-      })
+      lspconfig.lua_ls.setup({})
     end
   },
 }
