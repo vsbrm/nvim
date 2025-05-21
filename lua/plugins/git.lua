1b5d0e04674de4e932d0b9520907616c53b3e4b4
vsb <bormyshevv@gmail.com>
2025-05-21 21:01:31 +0300

    Configure gitsigns keybindings with which-key

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,17 +3,18 @@ return {
   config = function()
     local gs = require("gitsigns")
     gs.setup({})
-    vim.keymap.set('n', '<leader>ghs', gs.stage_hunk, {desc = "stage hunk"})
-    vim.keymap.set('n', '<leader>ghr', gs.reset_hunk, {desc = "reset hunk"})
-    vim.keymap.set('v', '<leader>ghs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc = "stage hunk"})
-    vim.keymap.set('v', '<leader>ghr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc = "reset hunk"})
-    vim.keymap.set('n', '<leader>gbs', gs.stage_buffer, {desc = "stage buffer"})
-    vim.keymap.set('n', '<leader>gbr', gs.reset_buffer, {desc = "reset buffer"})
-    vim.keymap.set('n', '<leader>ghp', gs.preview_hunk, {desc = "preview hunk"})
-    vim.keymap.set('n', '<leader>gbl', function() gs.blame_line{full=true} end, {desc = "blame line"})
-    vim.keymap.set('n', '<leader>gtb', gs.toggle_current_line_blame, {desc = "toggle blame"})
-    vim.keymap.set('n', '<leader>ghd', gs.diffthis, {desc = "diff hunk"})
-    vim.keymap.set('n', '<leader>ghD', function() gs.diffthis('~') end, {desc = "diffhis"})
-    vim.keymap.set('n', '<leader>gtd', gs.toggle_deleted, {desc = "toggle deleted"})
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>g", group = "git" },
+      { "<leader>gh", group = "git hunk" },
+      { "<leader>ghs", gs.stage_hunk, desc = "stage", mode = "n" },
+      { "<leader>ghr", gs.reset_hunk, desc = "reset", mode = "n" },
+      { "<leader>ghp", gs.preview_hunk, desc = "preview", mode = "n" },
+      { "<leader>gb", group = "git buffer" },
+      { "<leader>gbs", gs.stage_buffer, desc = "stage", mode = "n" },
+      { "<leader>gbr", gs.reset_buffer, desc = "reset", mode = "n" },
+      { "<leader>gt", group = "git toggle" },
+      { "<leader>gtb", gs.toggle_current_line_blame, desc = "blame", mode = "n" }
+    })
   end
 }
