d1034669228104b305c5aecd7ecff80fa9d661c1
vsb <bormyshevv@gmail.com>
2025-06-07 00:22:37 +0300

    Get rid of lua snippets module

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -3,24 +3,10 @@ return {
     "hrsh7th/cmp-nvim-lsp"
   },
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -34,7 +20,6 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
         }),
